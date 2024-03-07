25d22edc18df38aaef89a4374085d9e329ea7f5d
Youssef Ahmed <joetifa2003@gmail.com>
2024-03-07 20:54:14 +0200

    some blur

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -2,44 +2,45 @@
 local M = {}
 
 M.ui = {
-  theme = "aquarium",
-  lsp_semantic_tokens = false,
+	theme = "aquarium",
+	lsp_semantic_tokens = false,
 
-  nvdash = {
-    load_on_startup = true,
-  },
-  statusline = {
-    theme = "vscode_colored",
-    overriden_modules = function(modules)
-      table.insert(
-        modules,
-        6,
-        (function()
-          return "%#StText# " .. vim.api.nvim_buf_get_name(0)
-        end)()
-      )
-    end,
-  },
+	nvdash = {
+		load_on_startup = true,
+	},
+	statusline = {
+		theme = "vscode_colored",
+		overriden_modules = function(modules)
+			table.insert(
+				modules,
+				6,
+				(function()
+					return "%#StText# " .. vim.api.nvim_buf_get_name(0)
+				end)()
+			)
+		end,
+	},
 
-  hl_override = {
-    ["@parameter"] = {
-      fg = "white",
-    },
-    ["@punctuation.bracket"] = {
-      fg = "#e8cca7",
-    },
-    ["Repeat"] = {
-      fg = "purple",
-    },
-    ["Search"] = {
-      bg = "#e8cca7",
-    },
-  },
+	hl_override = {
+		["@parameter"] = {
+			fg = "white",
+		},
+		["@punctuation.bracket"] = {
+			fg = "#e8cca7",
+		},
+		["Repeat"] = {
+			fg = "purple",
+		},
+		["Search"] = {
+			bg = "#e8cca7",
+		},
+	},
 
-  extended_integrations = { "dap" },
+	extended_integrations = { "dap" },
+	transparency = true,
 }
 
 M.plugins = "custom.plugins"
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
