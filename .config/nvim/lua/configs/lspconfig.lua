6761b21f5d99e70a813c48ebb254039ab349183e
Youssef Ahmed <joetifa2003@gmail.com>
2024-03-16 19:34:39 +0200

    feat: add tailwind

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -6,6 +6,7 @@ local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
+	{ name = "tailwindcss", settings = {} },
 	{ name = "pyright", settings = {} },
 	{ name = "zls", settings = {} },
 	{ name = "rust_analyzer", settings = {} },
