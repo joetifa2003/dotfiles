3de79e07567f340bae38313465a86846fe0928ae
Youssef Ahmed <joetifa2003@gmail.com>
2025-08-30 20:23:20 +0300

    feat: update

--- .local/bin/wallpaper-select.sh
+++ .local/bin/wallpaper-select.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-selected_file=$(find ~/wallpapers/ -type f | fuzzel -d -w 100)
+selected_file=$(find ~/wallpapers/ -type f -not -name ".gitattributes" | fuzzel -d -w 100)
 
 hyprpanel sw $selected_file
