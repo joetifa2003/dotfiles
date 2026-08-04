63faccf44d67c693935362ace509a8b6dc90bbe3
Youssef Ahmed <joetifa2003@gmail.com>
2026-08-04 06:07:50 +0300

    feat: update

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,104 +1,104 @@
 return {
-  ["image"] = "/home/joetifa/wallpapers/cropped.jpg",
+  ["image"] = "/home/joetifa/wallpapers/2-flower-basket.jpg",
 
-  ["background"] = "rgba(18120eff)",
+  ["background"] = "rgba(19120cff)",
 
   ["error"] = "rgba(ffb4abff)",
 
   ["error_container"] = "rgba(93000aff)",
 
-  ["inverse_on_surface"] = "rgba(372f2aff)",
+  ["inverse_on_surface"] = "rgba(372f28ff)",
 
-  ["inverse_primary"] = "rgba(8d4f1eff)",
+  ["inverse_primary"] = "rgba(855318ff)",
 
-  ["inverse_surface"] = "rgba(eee0d9ff)",
+  ["inverse_surface"] = "rgba(efe0d5ff)",
 
-  ["on_background"] = "rgba(eee0d9ff)",
+  ["on_background"] = "rgba(efe0d5ff)",
 
   ["on_error"] = "rgba(690005ff)",
 
   ["on_error_container"] = "rgba(ffdad6ff)",
 
-  ["on_primary"] = "rgba(502400ff)",
+  ["on_primary"] = "rgba(4a2800ff)",
 
-  ["on_primary_container"] = "rgba(451f00ff)",
+  ["on_primary_container"] = "rgba(ffdcbeff)",
 
-  ["on_primary_fixed"] = "rgba(311400ff)",
+  ["on_primary_fixed"] = "rgba(2c1600ff)",
 
-  ["on_primary_fixed_variant"] = "rgba(703807ff)",
+  ["on_primary_fixed_variant"] = "rgba(693c00ff)",
 
-  ["on_secondary"] = "rgba(452a17ff)",
+  ["on_secondary"] = "rgba(402c18ff)",
 
-  ["on_secondary_container"] = "rgba(ffd9c1ff)",
+  ["on_secondary_container"] = "rgba(ffdcbeff)",
 
-  ["on_secondary_fixed"] = "rgba(2d1605ff)",
+  ["on_secondary_fixed"] = "rgba(291806ff)",
 
-  ["on_secondary_fixed_variant"] = "rgba(5f402bff)",
+  ["on_secondary_fixed_variant"] = "rgba(59422cff)",
 
-  ["on_surface"] = "rgba(eee0d9ff)",
+  ["on_surface"] = "rgba(efe0d5ff)",
 
-  ["on_surface_variant"] = "rgba(d8c2b6ff)",
+  ["on_surface_variant"] = "rgba(d5c3b5ff)",
 
-  ["on_tertiary"] = "rgba(00363dff)",
+  ["on_tertiary"] = "rgba(2b3410ff)",
 
-  ["on_tertiary_container"] = "rgba(002e34ff)",
+  ["on_tertiary_container"] = "rgba(dce8b4ff)",
 
-  ["on_tertiary_fixed"] = "rgba(001f24ff)",
+  ["on_tertiary_fixed"] = "rgba(161f01ff)",
 
-  ["on_tertiary_fixed_variant"] = "rgba(004f58ff)",
+  ["on_tertiary_fixed_variant"] = "rgba(414b24ff)",
 
-  ["outline"] = "rgba(a08d82ff)",
+  ["outline"] = "rgba(9d8e81ff)",
 
-  ["outline_variant"] = "rgba(53443aff)",
+  ["outline_variant"] = "rgba(51453aff)",
 
-  ["primary"] = "rgba(ffc59fff)",
+  ["primary"] = "rgba(fcb975ff)",
 
-  ["primary_container"] = "rgba(f3a36bff)",
+  ["primary_container"] = "rgba(693c00ff)",
 
-  ["primary_fixed"] = "rgba(ffdcc6ff)",
+  ["primary_fixed"] = "rgba(ffdcbeff)",
 
-  ["primary_fixed_dim"] = "rgba(ffb786ff)",
+  ["primary_fixed_dim"] = "rgba(fcb975ff)",
 
   ["scrim"] = "rgba(000000ff)",
 
-  ["secondary"] = "rgba(eabea2ff)",
+  ["secondary"] = "rgba(e1c1a4ff)",
 
-  ["secondary_container"] = "rgba(5f402bff)",
+  ["secondary_container"] = "rgba(59422cff)",
 
-  ["secondary_fixed"] = "rgba(ffdcc6ff)",
+  ["secondary_fixed"] = "rgba(ffdcbeff)",
 
-  ["secondary_fixed_dim"] = "rgba(eabea2ff)",
+  ["secondary_fixed_dim"] = "rgba(e1c1a4ff)",
 
   ["shadow"] = "rgba(000000ff)",
 
-  ["source_color"] = "rgba(f3a36bff)",
+  ["source_color"] = "rgba(764b1bff)",
 
-  ["surface"] = "rgba(18120eff)",
+  ["surface"] = "rgba(19120cff)",
 
-  ["surface_bright"] = "rgba(403833ff)",
+  ["surface_bright"] = "rgba(403830ff)",
 
-  ["surface_container"] = "rgba(251e1aff)",
+  ["surface_container"] = "rgba(261e18ff)",
 
-  ["surface_container_high"] = "rgba(302824ff)",
+  ["surface_container_high"] = "rgba(302822ff)",
 
-  ["surface_container_highest"] = "rgba(3b332fff)",
+  ["surface_container_highest"] = "rgba(3c332cff)",
 
-  ["surface_container_low"] = "rgba(211a16ff)",
+  ["surface_container_low"] = "rgba(211a14ff)",
 
-  ["surface_container_lowest"] = "rgba(130d09ff)",
+  ["surface_container_lowest"] = "rgba(130d07ff)",
 
-  ["surface_dim"] = "rgba(18120eff)",
+  ["surface_dim"] = "rgba(19120cff)",
 
-  ["surface_tint"] = "rgba(ffb786ff)",
+  ["surface_tint"] = "rgba(fcb975ff)",
 
-  ["surface_variant"] = "rgba(53443aff)",
+  ["surface_variant"] = "rgba(51453aff)",
 
-  ["tertiary"] = "rgba(85deedff)",
+  ["tertiary"] = "rgba(c0cc9aff)",
 
-  ["tertiary_container"] = "rgba(68c2d1ff)",
+  ["tertiary_container"] = "rgba(414b24ff)",
 
-  ["tertiary_fixed"] = "rgba(9af0ffff)",
+  ["tertiary_fixed"] = "rgba(dce8b4ff)",
 
-  ["tertiary_fixed_dim"] = "rgba(7ad4e3ff)",
+  ["tertiary_fixed_dim"] = "rgba(c0cc9aff)",
 
 }
--- .config/hypr/config/env.lua
+++ .config/hypr/config/env.lua
@@ -1,8 +1,6 @@
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
-hl.env("QT_QPA_PLATFORM", "wayland")
-hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
@@ -13,3 +11,19 @@ hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 hl.env("__VK_LAYER_NV_optimus", "NVIDIA_only")
+
+-- QT stuff
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QSG_RENDER_LOOP", "threaded")
+hl.env("QSG_RHI_BACKEND", "vulkan")
+hl.env("__GL_SYNC_TO_VBLANK", "0")
+
+hl.config({
+	misc = {
+		vrr = 0,
+	},
+	debug = {
+		vfr = false,
+	},
+})
